56fcab9351d103605ee9da67f0e448c4ea8f763e
Lucas <lucas.schwindenhammer@edu.ece.fr>
2025-03-28 16:37:33 +0100

    Deuxième commit - Petit code "helloword" pour test

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    // Méthode affichant le message "Hello world"
+    public void afficherMessage() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Création de l'objet HelloWorld
+        HelloWorld hello = new HelloWorld();
+
+        // Appel de la méthode pour afficher le message
+        hello.afficherMessage();
+    }
+}
